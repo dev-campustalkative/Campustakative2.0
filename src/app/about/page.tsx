0ba3120dfca18c54795e4a9cf80bc4442c96c8aa
kelani34 <kelanitaiwo34@gmail.com>
2023-06-28 18:46:26 +0100

    update: about header fix

---
+++ src/app/about/page.tsx
@@ -0,0 +1,14 @@
+"use client";
+
+import Container from "../components/Container";
+import AboutHeader from "../containers/AboutHeader";
+
+const About = () => {
+  return (
+    <Container>
+      <AboutHeader />
+    </Container>
+  );
+};
+
+export default About;
---
+++ src/app/containers/AboutHeader.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+const AboutHeader = () => {
+  return (
+    <div className="pt-36 ">
+      <div className="rounded-3xl bg-ct-secondary-1100 py-8 px-16">
+        <h2 className="text-ct-grey-100 text-5xl font-sora font-bold ">
+          Today connecting with like minded youths to share, learn, grow and
+          connect have become more chaotic.It’s time we build that community
+          focused on growth and development.
+        </h2>
+        <h1 className=" capitalize text-9xl font-extrabold opacity-80 pt-4">
+          ABOUT
+        </h1>
+      </div>
+    </div>
+  );
+};
+
+export default AboutHeader;
