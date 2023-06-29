4d8dd9bfa84d809128ebe02e4dcc2890a9ba1827
kelani34 <kelanitaiwo34@gmail.com>
2023-06-29 02:36:18 +0100

    update: mission section fix

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -3,12 +3,14 @@
 import Container from "../components/Container";
 import AboutHeader from "../containers/AboutHeader";
 import Journey from "../containers/Journey";
+import Mission from "../containers/Mission";
 
 const About = () => {
   return (
     <Container>
       <AboutHeader />
       <Journey />
+      <Mission />
     </Container>
   );
 };
