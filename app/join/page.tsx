63fd2e8d9c86fa3e2f8256634a01a78a25b5f313
kelani34 <kelanitaiwo34@gmail.com>
2023-07-30 17:43:35 +0100

    Update at Sun, 30 Jul 2023 16:43:35 GMT

---
+++ app/join/page.tsx
@@ -0,0 +1,14 @@
+"use client";
+
+import Container from "@/components/Container";
+import JoinHeader from "../../containers/JoinHeader";
+
+const Join = () => {
+  return (
+    <Container>
+      <JoinHeader />
+    </Container>
+  );
+};
+
+export default Join;
