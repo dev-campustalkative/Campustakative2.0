316767151866556aa39bf559ac5d254aa3d00e25
kelani34 <kelanitaiwo34@gmail.com>
2023-07-30 18:17:51 +0100

    Update at Sun, 30 Jul 2023 17:17:51 GMT

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import Container from "@/components/container";
-import Divider from "@/components/civider";
+import Divider from "@/components/divider";
 import ContactHeader from "@/containers/contact-header";
 import FAQ from "@/containers/faq";
 
