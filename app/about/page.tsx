fe6597daea83c6d014fd0b7c15a74afad69787fe
kelani34 <kelanitaiwo34@gmail.com>
2023-07-30 18:12:57 +0100

    Update at Sun, 30 Jul 2023 17:12:57 GMT

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
-import Container from "@/components/Container";
-import AboutHeader from "@/containers/AboutHeader";
+import Container from "@/components/container";
+import AboutHeader from "@/containers/about-header";
 import Journey from "@/containers/Journey";
 import Mission from "@/containers/Mission";
 import Team from "@/containers/Team";
