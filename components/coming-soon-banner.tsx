9883144e5a808b310751ea6d9ff1867faaf2f8f9
kelani34 <kelanitaiwo34@gmail.com>
2023-08-15 12:24:45 +0100

    Update at Tue, 15 Aug 2023 11:24:45 GMT

---
+++ components/coming-soon-banner.tsx
@@ -0,0 +1,13 @@
+// ComingSoonBanner.tsx
+
+const ComingSoonBanner: React.FC = () => {
+  return (
+    <div className="bg-yellow-500 text-white text-center py-2">
+      <p className="text-sm font-medium">
+        🚀 Exciting News: Our Website Is Coming Soon! Stay tuned for updates. 🚀
+      </p>
+    </div>
+  );
+};
+
+export default ComingSoonBanner;
