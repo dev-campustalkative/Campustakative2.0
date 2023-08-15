1a8f4a82c7c16ca7a3f2bb6a96a4c159c88e4d66
kelani34 <kelanitaiwo34@gmail.com>
2023-08-15 13:14:59 +0100

    Update at Tue, 15 Aug 2023 12:14:59 GMT

--- app/opportunities/page.tsx
+++ app/opportunities/page.tsx
@@ -1,3 +1,5 @@
+import { Card } from "@/components/ui/card";
+
 const page = () => {
   return (
     <div className="absolute w-full mx-auto h-full shadow-2xl z-10 bg-background/80 backdrop-blur-sm p-20 flex items-center justify-center">
