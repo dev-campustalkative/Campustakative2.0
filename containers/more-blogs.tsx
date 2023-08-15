4455ecc0babe876050b9c613c2acd71dc9f760d5
kelani34 <kelanitaiwo34@gmail.com>
2023-08-15 13:12:32 +0100

    Update at Tue, 15 Aug 2023 12:12:32 GMT

--- containers/more-blogs.tsx
+++ containers/more-blogs.tsx
@@ -2,10 +2,15 @@
 
 import Image from 'next/image';
 import BlogCard from '@/components/blog-card';
+import { Card } from '@/components/ui/card';
 
 const MoreBlogs = () => {
   return (
-    <div className="pb-36 md:pb-12">
+    <div className="pb-36 md:pb-12 relative">
+      <div className="absolute w-full mx-auto h-full shadow-2xl z-10 bg-background/80 backdrop-blur-sm p-20 flex items-center justify-center">
+        <Card className="p-20 text-2xl font-bold font-sora">Coming Soon</Card>
+      </div>
+
       <div className="py-4">
         <h2 className="text-3xl font-sora font-bold text-ct-grey-1000 md:text-xl max-w-[690px]">
           Our thoughts and observations regarding jobs, hiring{' '}
