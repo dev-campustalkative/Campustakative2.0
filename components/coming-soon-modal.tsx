c75798e4036f757e48d72d25add602b53af7e1cc
kelani34 <kelanitaiwo34@gmail.com>
2023-08-15 12:02:12 +0100

    Update at Tue, 15 Aug 2023 11:02:12 GMT

---
+++ components/coming-soon-modal.tsx
@@ -0,0 +1,18 @@
+'use client';
+
+import { Button } from '@/components/button';
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from '@/components/ui/dialog';
+
+const ComingSoonModal = () => {
+  return <div>ComingSoonModal</div>;
+};
+
+export default ComingSoonModal;
