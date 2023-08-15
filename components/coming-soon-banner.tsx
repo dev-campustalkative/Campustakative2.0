2d5e4e21f83aafd763e1865cd8fef52029900d28
kelani34 <kelanitaiwo34@gmail.com>
2023-08-15 12:36:15 +0100

    Update at Tue, 15 Aug 2023 11:36:15 GMT

--- components/coming-soon-banner.tsx
+++ components/coming-soon-banner.tsx
@@ -2,7 +2,7 @@ const ComingSoonBanner: React.FC<{ children: React.ReactNode }> = ({
   children,
 }) => {
   return (
-    <div className="fixed top-0 left-0 right-0 bg-gradient-to-b from-rose-400 to-pink-500 text-white text-center py-2 z-50 transform transition-transform animate-in">
+    <div className="fixed top-0 left-0 right-0 bg-gradient-to-b from-rose-400 to-pink-500 text-white text-center py-2 z-50 animate-slide-down">
       <p className="text-sm font-medium">{children}</p>
     </div>
   );
