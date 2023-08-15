f9fb162eab783312d7013f63b94cff1da2813574
kelani34 <kelanitaiwo34@gmail.com>
2023-08-15 12:27:30 +0100

    Update at Tue, 15 Aug 2023 11:27:29 GMT

--- components/coming-soon-banner.tsx
+++ components/coming-soon-banner.tsx
@@ -1,9 +1,22 @@
 const ComingSoonBanner: React.FC = () => {
   return (
-    <div className="bg-gradient-to-b from-rose-400 to-pink-500 text-white text-center py-2">
-      <p className="text-sm font-medium">
-        🚀 Exciting News: Our Website Is Coming Soon! Stay tuned for updates. 🚀
-      </p>
+    <div className="fixed top-0 left-0 right-0 bg-gradient-to-b from-rose-400 to-pink-500 text-white py-4 z-50">
+      <div className="max-w-6xl mx-auto px-4">
+        <div className="text-center">
+          <h2 className="text-3xl font-semibold mb-2">
+            🚀 Exciting News: Our Website Is Coming Soon!
+          </h2>
+          <p className="text-lg mb-4">
+            Stay tuned for updates as we prepare to launch our new and improved
+            website. We're excited to share a range of exciting features and
+            products with you!
+          </p>
+          <p className="text-sm">
+            Follow us on social media to stay up-to-date with our progress and
+            announcements.
+          </p>
+        </div>
+      </div>
     </div>
   );
 };
