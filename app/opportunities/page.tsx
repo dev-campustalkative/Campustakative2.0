fd6e972635ac4ef8bd095e18b5fc1f674fad409c
kelani34 <kelanitaiwo34@gmail.com>
2023-08-15 13:14:52 +0100

    Update at Tue, 15 Aug 2023 12:14:52 GMT

--- app/opportunities/page.tsx
+++ app/opportunities/page.tsx
@@ -1,5 +1,9 @@
 const page = () => {
-  return <div>page</div>;
+  return (
+    <div className="absolute w-full mx-auto h-full shadow-2xl z-10 bg-background/80 backdrop-blur-sm p-20 flex items-center justify-center">
+      <Card className="p-20 text-2xl font-bold font-sora">Coming Soon</Card>
+    </div>
+  );
 };
 
 export default page;
